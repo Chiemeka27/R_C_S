0c6b885ae88840686029148ce89663ae771078bc
Chiemeka27 <cotiono27@gmail.com>
2022-06-30 14:30:57 +0200

    update2 to file

--- index.js
+++ index.js
@@ -62,95 +62,113 @@ function player(playerChoice) {
 player()
 
 */
+let choice = " ";
 function computerPlay(){
     let num = randomNumber = Math.floor(Math.random()*3) + 1;
-    let choice = " ";
+  
     if (num == 1) {
         return choice = "rock"
     } else if ( num == 2) {
-        return choice = " paper"
-    } else {
+        return choice = "paper"
+    } else  if (num == 3){
         return choice = "scissors"
     }
     
 }
-
+let decision = " "
 function player() {
     let num = randomNumber = Math.floor(Math.random()*3) + 1;
-    let decision = " "
+    
     if (num == 1) {
         return decision = "rock"
     } else if (num == 2) {
         return decision = "paper"
-    } else  {
+    } else  if ( num == 3){
         return decision = "scissors"
     }
 }
 
 
 
-let playerSelection = player();
-let computerSelection = computerPlay();
-let game = ""
-function me(){
+
+
+
+
+
+ /*
+
+function me( ){
     
 if ( playerSelection == "rock"  &&  computerSelection == "scissors") {
-    return  game = " You win, rock beats scissors";
+    return  " You win, rock beats scissors";
 } 
 else if ( playerSelection == "rock" &&  computerSelection == "paper") {
-        return game = "You lose, paper beats rock";
+        return "You lose, paper beats rock";
 } 
 else if ( playerSelection == "rock" &&  computerSelection == "rock") {
-    return game = "no winner,rr";
+    return "no winner,rr";
 } 
 else if ( playerSelection == "scissors"  &&  computerSelection == "rock" ) {
-        return game = "You lose, rock beats scissors";
+        return  "You lose, rock beats scissors";
 } 
 else if ( playerSelection == "scissors"  &&  computerSelection == "paper" ) {
-       return game = "You win, scissors beats paper";
+       return  "You win, scissors beats paper";
 } 
 else if ( playerSelection == "scissors"  &&  computerSelection == "scissors" ) {
-    return game = "no winner, ss";
+    return  "no winner, ss";
 } 
 else if ( playerSelection == "paper"  &&  computerSelection == "rock" ) {
-        return game = "You win, paper beats rock";
+        return "You win, paper beats rock";
 } 
 else if ( playerSelection == "paper"  &&  computerSelection == "scissors") {
-       return game = "You lose, scissors beats paper ";
+       return  "You lose, scissors beats paper ";
 } 
 else if ( playerSelection == "paper"  &&  computerSelection == "paper" ) {
-    return game = "no winner, pp";
+    return  "no winner, pp";
 } 
 else {
     console.log("nahhhhhh")
 }
 }
+*/
+function playRound(playerSelection, computerSelection) {
+    if ( playerSelection == "rock"  &&  computerSelection == "scissors") {
+        return  " You win, rock beats scissors";
+    } 
+    else if ( playerSelection == "rock" &&  computerSelection == "paper") {
+            return "You lose, paper beats rock";
+    } 
+    else if ( playerSelection == "rock" &&  computerSelection == "rock") {
+        return "no winner,rr";
+    } 
+    else if ( playerSelection == "scissors"  &&  computerSelection == "rock" ) {
+            return  "You lose, rock beats scissors";
+    } 
+    else if ( playerSelection == "scissors"  &&  computerSelection == "paper" ) {
+           return  "You win, scissors beats paper";
+    } 
+    else if ( playerSelection == "scissors"  &&  computerSelection == "scissors" ) {
+        return  "no winner, ss";
+    } 
+    else if ( playerSelection == "paper"  &&  computerSelection == "rock" ) {
+            return "You win, paper beats rock";
+    } 
+    else if ( playerSelection == "paper"  &&  computerSelection == "scissors") {
+           return  "You lose, scissors beats paper ";
+    } 
+    else if ( playerSelection == "paper"  &&  computerSelection == "paper" ) {
+        return  "no winner, pp";
+    } 
+    else {
+        console.log("nahhhhhh")
+    }
 
-console.log(me())
+
+
+  }
+ 
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound())
 console.log(playerSelection);
 console.log(computerSelection)
-console.log(game)
-
- /*if ( playerSelection == "rock"  &&  computerSelection == "scissors") {
-    return  game = " You win, rock beats scissors"
-} 
-else if ( playerSelection == "rock" &&  computerSelection == "paper") {
-        return game = "You lose, paper beats rock"
-} 
-else if ( playerSelection == "scissors"  &&  computerSelection == "rock" ) {
-        return game = " You lose, rock beats rock beats scissors"
-} 
-else if ( playerSelection == "scissors"  &&  computerSelection == "paper" ) {
-       return game = "You win, scissors beats paper"
-} 
-else if ( playerSelection == "paper"  &&  computerSelection == "rock" ) {
-        return game = "You win, paper beats rock"
-} 
-else if ( playerSelection == "paper"  &&  computerSelection == "scissors") {
-       return game = "You lose, scissors beats paper "
-} 
-else {
-       return game = "No winner, same answer"
-
-}
-*/
